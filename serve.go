1aeed02ecb8ebf790ec7d0e0ddb35ca0924785e8
Peter Hellberg <peter@c7.se>
2014-04-13 03:13:27 +0200

    Default to binding on 0.0.0.0 instead of 127.0.0.1

--- serve.go
+++ serve.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	port = flag.String("port", "127.0.0.1:9000", "Define what TCP port to bind to")
+	port = flag.String("port", "0.0.0.0:9000", "Define what TCP port to bind to")
 	root = flag.String("root", ".", "Define the root filesystem path")
 )
 
