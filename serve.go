7815c35677e531fd0d20405de7a36a0270307aaf
Peter Hellberg <peter@c7.se>
2024-07-16 17:45:30 +0200

    serve: Extract run function

--- serve.go
+++ serve.go
@@ -3,21 +3,40 @@ package main
 import (
 	"flag"
 	"net/http"
+	"os"
 )
 
-var (
-	addr = flag.String("addr", "0.0.0.0:9000", "Define what TCP address (host:port) to bind to")
-	root = flag.String("root", ".", "Define the root filesystem path")
-)
+type input struct {
+	addr string
+	root string
+}
+
+func parseInput(args []string) (input, error) {
+	var in input
+
+	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+
+	flags.StringVar(&in.addr, "addr", "localhost:9000", "Define what TCP address (host:port) to bind to")
+	flags.StringVar(&in.root, "root", ".", "Define the root filesystem path")
+
+	return in, flags.Parse(args[1:])
+}
 
 func main() {
-	flag.Parse()
-
-	go func() {
-		print("Serving '", *root, "' on http://", *addr, "\n")
-	}()
-
-	if err := http.ListenAndServe(*addr, http.FileServer(http.Dir(*root))); err != nil {
+	if err := run(os.Args); err != nil {
 		print(err.Error() + "\n")
 	}
+}
+
+func run(args []string) error {
+	in, err := parseInput(args)
+	if err != nil {
+		return err
+	}
+
+	print("Serving '", in.root, "' on http://", in.addr, "\n")
+
+	fs := http.FileServer(http.Dir(in.root))
+
+	return http.ListenAndServe(in.addr, fs)
 }
